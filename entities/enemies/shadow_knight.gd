e2f826f2fe95fd02b371621889856aba4a7e0d9e
PITT1 <petroleo.plta.97@gmail.com>
2024-10-07 20:26:41 -0400

    haciendo desaparecer y aparecer al shadow knight

--- entities/enemies/shadow_knight.gd
+++ entities/enemies/shadow_knight.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-var vanish_velocity = 0.1
-
+var opacities = [0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0]
+var reverseOpacities = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+var player: CharacterBody2D = null
 
 func _ready() -> void:
 	anim.play("idle")
@@ -16,8 +17,22 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
-func _on_vision_area_body_entered(body: Node2D) -> void:
-	vanished()
+func _on_vision_area_body_entered(body: CharacterBody2D) -> void:
+	player = body
+	disappear()
+	await get_tree().create_timer(2).timeout
+	global_position.x = player.global_position.x - 10
+	appear()
 	
-func vanished():
-	set_modulate(Color(1, 1, 1, 0.7))
+	
+
+func disappear():
+	for i in range(len(opacities)):
+		set_modulate(Color(1, 1, 1, opacities[i]))
+		await get_tree().create_timer(0.2).timeout
+		
+func appear():
+	for i in range(len(reverseOpacities)):
+		set_modulate(Color(1, 1, 1, reverseOpacities[i]))
+		await get_tree().create_timer(0.2).timeout
+	
