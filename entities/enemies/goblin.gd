1e0efe96b9c69f9f0468824ae87ffe553cd6fc19
PITT1 <petroleo.plta.97@gmail.com>
2024-10-12 20:48:18 -0400

    haciendo que los goblins detecten caidas

--- entities/enemies/goblin.gd
+++ entities/enemies/goblin.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var vision_area: Area2D = $visionArea
+@onready var flee_area: Area2D = $fleeArea
 
 var attack = false
 var death = false
 var idle = false
 var run = false
 var takeHit = false
+
+var player = null
+
+var flee = false
+
+var isFallRight = false
+var isFallLeft = false 
+@export var speed: float = 80
+@export var acceleration: float = 300
+
+@export var proyectile: PackedScene
 
 
 func _ready() -> void:
@@ -31,7 +44,37 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
+	if flee:
+		if player.global_position > global_position and not isFallLeft:
+			velocity.x -= acceleration * delta
+			if velocity.x < -speed:
+				velocity.x = -speed
+		elif player.global_position < global_position and not isFallRight:
+			velocity.x += acceleration * delta
+			if velocity.x > speed:
+				velocity.x = speed
+	elif not flee:
+		if velocity.x > 1:
+			velocity.x -= acceleration * delta
+			if velocity.x < 1:
+				velocity.x = 0
+		elif velocity.x < -1:
+			velocity.x += acceleration * delta
+			if velocity.x > -1:
+				velocity.x = 0
+				
+	if velocity.x > 1 and not attack:
+		anim.flip_h = false
+		on_run()
+	elif velocity.x < -1 and not attack:
+		anim.flip_h = true
+		on_run()
+		
+	if isFallLeft or isFallRight:
+		flee_area.set_monitoring(false)
+	else:
+		flee_area.set_monitoring(true)
 	move_and_slide()
 
 
@@ -71,4 +114,47 @@ func on_takeHit():
 	takeHit = true
 
 func _on_vision_area_body_entered(body: CharacterBody2D) -> void:
-	pass
+	player = body
+	if not flee:
+		if player.global_position > global_position:
+			anim.flip_h = false
+		else:
+			anim.flip_h = true
+		on_attack()
+	
+
+func _on_flee_area_body_entered(body: Node2D) -> void:
+	flee = true
+
+func _on_flee_area_body_exited(body: Node2D) -> void:
+	flee = false
+	vision_area.set_monitoring(false)
+	vision_area.set_monitoring(true)
+
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	if anim.get_animation() == "attack" and anim.get_frame() == 6:
+		launchingProyectile()
+		on_idle()
+
+
+func launchingProyectile():
+	vision_area.scale = Vector2(5, 5)
+	var instantiated = proyectile.instantiate()
+	add_sibling(instantiated)
+	instantiated.global_position = global_position + Vector2(0, -10)
+	instantiated.direction = global_position.direction_to(player.global_position)
+	await get_tree().create_timer(2).timeout
+	vision_area.set_monitoring(false)
+	vision_area.scale = Vector2(1, 1)
+	vision_area.set_monitoring(true)
+
+
+func _on_sensor_right_body_exited(body: Node2D) -> void:
+	if body.name == "TileMapLayer":
+		isFallRight = true 
+
+
+func _on_sensor_left_body_exited(body: Node2D) -> void:
+	if body.name == "TileMapLayer":
+		isFallLeft = true 
--- entities/proyectiles/goblin_proyectil.gd
+++ entities/proyectiles/goblin_proyectil.gd
@@ -1,11 +1,26 @@
 extends Node2D
+@onready var collision_area: Area2D = $collisionArea
+@onready var anim: AnimationPlayer = $AnimationPlayer
+
+var direction = Vector2(0, 0)
+@export var velocity: float = 200
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	if direction.x > 0:
+		anim.play("rotate_Right")
+	else:
+		anim.play("rotate_Left")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position += direction * delta * velocity
+
+
+func _on_collision_area_body_entered(body: Node2D) -> void:
+	if body.name == "CharacterBody2D":
+		body.lives -= 1
+		body.gettingHit = true
+	
+	queue_free()
+	direction = Vector2(0, 0)
