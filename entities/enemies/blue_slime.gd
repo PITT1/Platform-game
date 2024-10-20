9489cb0de3a3bf11a3053fc0a1687a05cff1633a
PITT1 <petroleo.plta.97@gmail.com>
2024-10-19 21:27:03 -0400

    creando muerte y ataque de los slimes

--- entities/enemies/blue_slime.gd
+++ entities/enemies/blue_slime.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var timer: Timer = $Timer
+@onready var hit_area_collision_shape: CollisionShape2D = $hitArea/hitArea_CollisionShape
 
 
 var player = null
 var on_air = false
+
+@export var lives: float = 3
+var gettingHit = false 
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -25,7 +29,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = 0
 		anim.play("idle")
 		on_air = false
-
+		
+	if lives < 1:
+		queue_free()
+		
+	if gettingHit:
+		timer.start(1)
+		set_modulate(Color(100, 100, 100))
+		await get_tree().create_timer(0.1).timeout
+		set_modulate(Color(1, 1, 1))
+		gettingHit = false
 
 	move_and_slide()
 
@@ -53,3 +66,11 @@ func jump():
 	elif player.global_position.x < global_position.x:
 		velocity.y = -350
 		anim.flip_h = true
+
+
+func _on_hit_area_body_entered(body: CharacterBody2D) -> void:
+	body.gettingHit = true
+	body.lives -= 1
+	hit_area_collision_shape.set_disabled(true)
+	await get_tree().create_timer(0.5).timeout
+	hit_area_collision_shape.set_disabled(false)
