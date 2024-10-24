eca2fba5519c7785709996ef6c976d083379bb4d
PITT1 <petroleo.plta.97@gmail.com>
2024-10-23 22:26:55 -0400

    creando nivel tutorial

--- obstacles/poizon_ivi2.gd
+++ obstacles/poizon_ivi2.gd
@@ -1,13 +1,9 @@
 extends Area2D 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_body_entered(body: Node2D) -> void:
-	body.gettingHit = true
-	body.lives -= 1
-	body.velocity.y = -200
-	
+	if not body.death:
+		body.gettingHit = true
+		body.lives -= 1
+		body.velocity.y = -200
+	else:
+		pass
