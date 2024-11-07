3b855b00efce7e65d326138a6e78b97b716e1dae
PITT1 <petroleo.plta.97@gmail.com>
2024-11-06 22:56:08 -0400

    flyeyes listos

--- entities/enemies/fly_eye.gd
+++ entities/enemies/fly_eye.gd
@@ -3,14 +3,41 @@ extends CharacterBody2D
 var player : CharacterBody2D = null
 @export var speed: float = 60
 @export var acceleration: float = 300
+@export var lives: float = 3
+@export var death_particles: PackedScene
+var gettingHit = false
+@onready var hit_box: CollisionShape2D = $CollisionShape2D
+@onready var hit_area_collision: CollisionShape2D = $hitArea/hitArea_collision
 
 func _ready() -> void:
 	anim.play("fly")
 
 func _physics_process(delta: float) -> void:
-	if player != null:
+	if player != null and not player.death and not gettingHit:
 		movement(delta)
+	if player:
+		if player.death:
+			velocity = Vector2.ZERO
 		
+	if lives < 1:
+		queue_free()
+		if death_particles:
+			var instantiated_particles = death_particles.instantiate()
+			add_sibling(instantiated_particles)
+			instantiated_particles.global_position = global_position
+		
+	if gettingHit:
+		anim.play("gettingHit")
+		
+	if player:
+		if player.global_position.x > global_position.x:
+			anim.flip_h = false
+			hit_box.set_position(Vector2(6, 3))
+			hit_area_collision.set_position(Vector2(6, 3))
+		else:
+			anim.flip_h = true
+			hit_box.set_position(Vector2(-6, 3))
+			hit_area_collision.set_position(Vector2(-6, 3))
 		
 	move_and_slide()
 
@@ -38,3 +65,15 @@ func movement(delta):
 		velocity += Vector2.UP * acceleration * delta
 		if velocity.y < -speed:
 			velocity.y = -speed
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.get_animation() == "gettingHit":
+		gettingHit = false
+		anim.play("fly")
+
+
+func _on_hit_area_body_entered(body: Node2D) -> void:
+	body.gettingHit = true
+	body.lives -= 1
+	velocity = body.global_position.direction_to(global_position) * 200
