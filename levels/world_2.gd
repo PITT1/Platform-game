ba78a3019633096711d0af8f95990e78a44b04dc
PITT1 <petroleo.plta.97@gmail.com>
2024-11-27 19:02:16 -0400

    creando pantalla de victoria

--- levels/world_2.gd
+++ levels/world_2.gd
@@ -1,12 +1,6 @@
 extends Node2D
-@onready var player: Node2D = $player
-@onready var leaf_particles: CPUParticles2D = $leaf_particles
+@export var is_level_pass: bool = false 
 
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-func _physics_process(delta: float) -> void:
-	print(player.get_position())
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	if body:
+		is_level_pass = true
