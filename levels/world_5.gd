0f058b75631e9463e34421c67c9998d4f75badb4
PITT1 <petroleo.plta.97@gmail.com>
2024-12-04 19:56:35 -0400

    todavia con el nivel 5

--- levels/world_5.gd
+++ levels/world_5.gd
@@ -1,7 +1,26 @@
 extends Node2D
 
+@export var is_level_pass: bool = false 
+@onready var player: Node2D = $player
 
+var scene_game_over = preload("res://hud/game_over.tscn")
+var scene_you_win = preload("res://hud/you_win_hud.tscn")
 
+var has_executed_once = false
+
+func _process(delta: float) -> void:
+	var playerChild = player.get_children()
+	if not has_executed_once:
+		if playerChild[0].lives < 1:
+			var instantia = scene_game_over.instantiate()
+			add_child(instantia)
+			instantia.showHud(true)
+			set_has_executed_once()
+	if delta:
+		pass
+
+func set_has_executed_once():
+	has_executed_once = true
 
 func _on_camera_move_1_body_entered(body: CharacterBody2D) -> void:
 	var pcamPlayer = body.get_child(5).get_child(0)
