d066b7938bbd92f1b1b45b4d35ae20bceabab00a
PITT1 <petroleo.plta.97@gmail.com>
2024-12-04 22:28:21 -0400

    terminando el nivel 5

--- levels/world_5.gd
+++ levels/world_5.gd
@@ -50,3 +50,21 @@ func _on_camera_move_5_body_entered(body: CharacterBody2D) -> void:
 
 func _on_death_zone_body_entered(body: CharacterBody2D) -> void:
 	body.lives -= 5
+
+
+func _on_camera_move_6_body_entered(body: CharacterBody2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 130))
+
+
+func _on_camera_move_6_body_exited(body: CharacterBody2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 0))
+
+
+func _on_you_win_area_body_entered(body: CharacterBody2D) -> void:
+	if body:
+		is_level_pass = true
+		var instantia = scene_you_win.instantiate()
+		add_child(instantia)
+		instantia.showHud(true)
