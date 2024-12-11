fb35d2588ceb682b5baffa8f6f5015aa02529120
PITT1 <petroleo.plta.97@gmail.com>
2024-12-11 00:37:10 -0400

    lectura listo, pero escritura no, en json

--- hud/menu_levels.gd
+++ hud/menu_levels.gd
@@ -1,16 +1,42 @@
 extends Control
 
-@export var level_tutorial: PackedScene
-@export var level_1: PackedScene
-@export var level_2: PackedScene
-@export var level_3: PackedScene
-@export var level_4: PackedScene
-@export var level_5: PackedScene
+@onready var nivel_1_btn: Button = $ColorRect/VBoxContainer/nivel_1_btn
+@onready var nivel_2_btn: Button = $ColorRect/VBoxContainer/nivel_2_btn
+@onready var nivel_3_btn: Button = $ColorRect/VBoxContainer/nivel_3_btn
+@onready var nivel_4_btn: Button = $ColorRect/VBoxContainer/nivel_4_btn
+@onready var nivel_5_btn: Button = $ColorRect/VBoxContainer/nivel_5_btn
+
+var level_config_path: String = "res://levels/level_config/level_config.json"
 
 func _ready() -> void:
-	var tutorial = preload("res://levels/tutorial.tscn")
-	print(tutorial)
-
+	var file = FileAccess.open(level_config_path,FileAccess.READ)
+	var parsed_file = JSON.parse_string(file.get_as_text())
+	if parsed_file.level_1.is_level_bloqued:
+		nivel_1_btn.set_disabled(true)
+	else:
+		nivel_1_btn.set_disabled(false)
+	
+	if parsed_file.level_2.is_level_bloqued:
+		nivel_2_btn.set_disabled(true)
+	else:
+		nivel_2_btn.set_disabled(false)
+	
+	if parsed_file.level_3.is_level_bloqued:
+		nivel_3_btn.set_disabled(true)
+	else:
+		nivel_3_btn.set_disabled(false)
+	
+	if parsed_file.level_4.is_level_bloqued:
+		nivel_4_btn.set_disabled(true)
+	else:
+		nivel_4_btn.set_disabled(false)
+	
+	if parsed_file.level_5.is_level_bloqued:
+		nivel_5_btn.set_disabled(true)
+	else:
+		nivel_5_btn.set_disabled(false)
+	
+	file.close()
 
 func _on_tutorial_btn_button_up() -> void:
 	get_tree().change_scene_to_file("res://levels/tutorial.tscn")
--- levels/tutorial.gd
+++ levels/tutorial.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-@export var is_level_pass: bool = false 
-@export var is_level_bloqued: bool = true
 @onready var player: Node2D = $player
 var scene_game_over = preload("res://hud/game_over.tscn")
 var scene_you_win = preload("res://hud/you_win_hud.tscn")
+
+var level_config_path: String = "res://levels/level_config/level_config.json"
 
 var has_executed_once = false
 
@@ -25,7 +25,12 @@ func set_has_executed_once():
 
 func _on_win_area_body_entered(body: CharacterBody2D) -> void:
 	if body:
-		is_level_pass = true
+		var file = FileAccess.open(level_config_path,FileAccess.WRITE)
+		var parsed_file = JSON.parse_string(file.get_as_text())
+		parsed_file.tutorial.is_level_pass = true
+		parsed_file.level_1.is_level_bloqued = false
+		file.close()
+		
 		var instantia = scene_you_win.instantiate()
 		add_child(instantia)
 		instantia.showHud(true)
