9923feb632c73be928511c301e7b0515e0a48bf1
PITT1 <petroleo.plta.97@gmail.com>
2024-12-14 15:56:57 -0400

    arreglando las areas 2d en el level 4

--- levels/world_4.gd
+++ levels/world_4.gd
@@ -21,7 +21,13 @@ func set_has_executed_once():
 	has_executed_once = true
 
 
-func _on_win_area_body_entered(body: CharacterBody2D) -> void:
+
+func _on_death_zone_body_entered(body: CharacterBody2D) -> void:
+	body.lives = 0
+
+
+
+func _on_you_win_area_body_entered(body: Node2D) -> void:
 	if body:
 		SaveGameProcesor.save_data_levels("level_4", "level_5")
 		var instantia = scene_you_win.instantiate()
@@ -29,21 +35,37 @@ func _on_win_area_body_entered(body: CharacterBody2D) -> void:
 		instantia.showHud(true)
 
 
-func _on_move_camera_area_body_entered(body: CharacterBody2D) -> void:
+func _on_camera_move_1_body_entered(body: Node2D) -> void:
 	var pcamPlayer = body.get_child(5).get_child(0)
 	pcamPlayer.set_follow_offset(Vector2(0, 0))
 	pcamPlayer.set_limit_bottom(450)
 
 
-func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+func _on_camera_move_2_body_entered(body: Node2D) -> void:
 	var pcamPlayer = body.get_child(5).get_child(0)
 	pcamPlayer.set_follow_offset(Vector2(0, 100))
 
 
-func _on_area_2d_body_exited(body: CharacterBody2D) -> void:
+func _on_camera_move_3_body_entered(body: Node2D) -> void:
 	var pcamPlayer = body.get_child(5).get_child(0)
 	pcamPlayer.set_follow_offset(Vector2(0, 0))
 
 
-func _on_death_zone_body_entered(body: CharacterBody2D) -> void:
-	body.lives = 0
+func _on_camera_move_4_body_entered(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 100))
+
+
+func _on_camera_move_5_body_entered(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 0))
+
+
+func _on_camera_move_6_body_entered(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 100))
+
+
+func _on_camera_move_6_body_exited(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 0))
