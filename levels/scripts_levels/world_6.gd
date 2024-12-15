6fbde242626b654637cec5fbf0c0873ebb39e52f
PITT1 <petroleo.plta.97@gmail.com>
2024-12-15 18:43:05 -0400

    movimientos de camara en el nivel 6

---
+++ levels/scripts_levels/world_6.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+
+
+func _on_move_camera_1_body_entered(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(100, 120))
+
+	
+func _on_move_camera_1_body_exited(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 0))
+
+
+func _on_move_camera_0_body_entered(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_limit_bottom(450)
