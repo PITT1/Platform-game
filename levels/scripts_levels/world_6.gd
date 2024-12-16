1d233ab4b625e469e3d0f187a62647b5aaa584c9
PITT1 <petroleo.plta.97@gmail.com>
2024-12-15 20:54:37 -0400

    mas plataformas para el nivel 6

--- levels/scripts_levels/world_6.gd
+++ levels/scripts_levels/world_6.gd
@@ -16,3 +16,13 @@ func _on_move_camera_1_body_exited(body: Node2D) -> void:
 func _on_move_camera_0_body_entered(body: Node2D) -> void:
 	var pcamPlayer = body.get_child(5).get_child(0)
 	pcamPlayer.set_limit_bottom(450)
+
+
+func _on_move_camera_2_body_entered(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(50, 100))
+
+
+func _on_move_camera_2_body_exited(body: Node2D) -> void:
+	var pcamPlayer = body.get_child(5).get_child(0)
+	pcamPlayer.set_follow_offset(Vector2(0, 0))
