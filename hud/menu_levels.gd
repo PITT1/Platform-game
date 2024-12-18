210505f67088222d275dae24de48f90ae5346ba1
PITT1 <petroleo.plta.97@gmail.com>
2024-12-17 22:41:38 -0400

    traduciendo el menu de niveles

--- hud/menu_levels.gd
+++ hud/menu_levels.gd
@@ -1,11 +1,13 @@
 extends Control
 
+@onready var label: Label = $ColorRect/VBoxContainer/Label
 @onready var nivel_1_btn: Button = $ColorRect/VBoxContainer/nivel_1_btn
 @onready var nivel_2_btn: Button = $ColorRect/VBoxContainer/nivel_2_btn
 @onready var nivel_3_btn: Button = $ColorRect/VBoxContainer/nivel_3_btn
 @onready var nivel_4_btn: Button = $ColorRect/VBoxContainer/nivel_4_btn
 @onready var nivel_5_btn: Button = $ColorRect/VBoxContainer/nivel_5_btn
 @onready var nivel_6_btn: Button = $ColorRect/VBoxContainer/nivel_6_btn
+@onready var back: Button = $ColorRect/back
 
 var data_load: String
 var data_json
@@ -14,6 +16,25 @@ var data_json
 func _ready() -> void:
 	data_load = SaveGameProcesor.load_data()
 	data_json = JSON.parse_string(data_load)
+	var lang = SaveGameProcesor.load_language()
+	if lang == "spanish":
+		label.set_text(SaveGameProcesor.translate.menu_levels.label_title.es)
+		nivel_1_btn.set_text(SaveGameProcesor.translate.menu_levels.level_1.es)
+		nivel_2_btn.set_text(SaveGameProcesor.translate.menu_levels.level_2.es)
+		nivel_3_btn.set_text(SaveGameProcesor.translate.menu_levels.level_3.es)
+		nivel_4_btn.set_text(SaveGameProcesor.translate.menu_levels.level_4.es)
+		nivel_5_btn.set_text(SaveGameProcesor.translate.menu_levels.level_5.es)
+		nivel_6_btn.set_text(SaveGameProcesor.translate.menu_levels.level_6.es)
+		back.set_text(SaveGameProcesor.translate.menu_levels.btn_back.es)
+	elif lang == "english":
+		label.set_text(SaveGameProcesor.translate.menu_levels.label_title.en)
+		nivel_1_btn.set_text(SaveGameProcesor.translate.menu_levels.level_1.en)
+		nivel_2_btn.set_text(SaveGameProcesor.translate.menu_levels.level_2.en)
+		nivel_3_btn.set_text(SaveGameProcesor.translate.menu_levels.level_3.en)
+		nivel_4_btn.set_text(SaveGameProcesor.translate.menu_levels.level_4.en)
+		nivel_5_btn.set_text(SaveGameProcesor.translate.menu_levels.level_5.en)
+		nivel_6_btn.set_text(SaveGameProcesor.translate.menu_levels.level_6.en)
+		back.set_text(SaveGameProcesor.translate.menu_levels.btn_back.en)
 	
 	if not all_levels_unlock == true:	
 		if data_json.level_1.is_level_blocked:
