dfb0af5a59496fd7bf2e0b2fe92b8133b8b5c66e
PITT1 <petroleo.plta.97@gmail.com>
2025-01-06 19:27:39 -0400

    haciendo el contador de monedas

--- hud/lives_hud.gd
+++ hud/lives_hud.gd
@@ -1,9 +1,14 @@
 extends Control
 @onready var heart: TextureRect = $CanvasLayer/HBoxContainer/TextureRect
 @onready var heartContainer: HBoxContainer = $CanvasLayer/HBoxContainer
+@onready var anim: AnimatedSprite2D = $CanvasLayer/HBoxContainer2/AnimatedSprite2D
+@onready var coin_label: Label = $CanvasLayer/HBoxContainer2/Label
 
 var heartsNum
 var heartChange
+
+var total_coins: int
+var coins_num: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,6 +18,13 @@ func _ready() -> void:
 		var new_heart = heart.duplicate()
 		new_heart.name = "Heart_" + str(i)
 		heartContainer.add_child(new_heart)
+	
+	anim.play("coin")
+	
+	for i in get_parent().get_child_count():
+		if get_parent().get_child(i).name == "coins":
+			total_coins = get_parent().get_child(i).get_child_count()
+			coin_label.set_text("0/" + str(total_coins))
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
